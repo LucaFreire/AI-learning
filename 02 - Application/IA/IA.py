ef27767e13e6429e0a0b34da071eddf106a03e22
LucaFreire <lucasrodifreire@gmail.com>
2023-11-14 11:21:16 -0300

    add: fill_columns to use the naive bayes algorithm

--- 02 - Application/IA/IA.py
+++ 02 - Application/IA/IA.py
@@ -1,61 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("data\emotions.csv")
+
+cry_words = ["cry", "tears", "sob"]
+sadness_words = ["sad", "bad", "waiting"]
+happy_words = ["happy", "happiness", "sob" ]
+
+columns_words = {
+    "cry" : cry_words,
+    "happy": happy_words,
+    "sadness": sadness_words
+}
+
+def encode_sentiment():
+    le = LabelEncoder()
+    target = df["sentiment"]
+    Y = pd.DataFrame(target)
+    labels = le.fit_transform(Y)
+    return labels
+
+def include_word(full_phrase, keywords):
+    return 1 if full_phrase.lower() in keywords else 0
+
+def fill_columns(columns_words):
+    for key, keywords in columns_words.items():
+        df[key] = df["content"].apply(lambda full_phrase : include_word(full_phrase, keywords))
 
 
-
-
-
-# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.01, random_state=800)
-
-# model = DecisionTreeClassifier()
-# model.fit(X_train, Y_train)
-
-# Y_pred = model.predict(X_test)
-# accuracy = accuracy_score(Y_test, Y_pred)
-# print(accuracy)
-
-# confusion_matrix(Y_test, Y_pred)
-# plot_tree(model)
-
-
-
-
-
-# print("yeah")
-# df = pd.read_csv("data\emotions.csv")
-
-# def data_cleanup(num):
-#     df.head(num)
-#     df.fillna(0)
-
-# def encode_sentiment():
-#     le = LabelEncoder()
-#     target = df["sentiment"]
-#     Y = pd.DataFrame(target)
-#     labels = le.fit_transform(Y)
-#     return labels
-
-# def training():
-#     data_cleanup()
-#     data = df[["content"]]
-#     X = pd.DataFrame(data)
-
-#     Y = encode_sentiment()
-
-#     vectorizer = TfidfVectorizer()
-#     X_train_tfidf_vectorize = vectorizer.fit_transform(X)
-
-#     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(70), random_state=1, verbose=True)
-#     clf.fit(X_train_tfidf_vectorize, Y)
-
-#     phrase_test = ["Im very sad"]
-#     X_new_tfidf_vectorize = vectorizer.transform(phrase_test)
-
-#     pred = clf.predict(X_new_tfidf_vectorize)
-
-#     for doc, category in zip(phrase_test, pred):
-#         print('%r => %s' % (doc, category))
-# print("Came")
-# training()
-
-
-
-
+fill_columns(columns_words)
--- 02 - Application/IA/main.py
+++ 02 - Application/IA/main.py
@@ -1,38 +1,4 @@
-import numpy as np
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
+import IA
 
 
-df = pd.read_csv("data\emotions.csv")
-
-
-cry_words = ["cry", "tears", "sob"]
-sadness_words = ["sad", "bad", "waiting"]
-happy_words = ["happy", "happiness", "sob" ]
-
-columns_words = {
-    "cry" : cry_words,
-    "happy": happy_words,
-    "sadness": sadness_words
-}
-
-def encode_sentiment():
-    le = LabelEncoder()
-    target = df["sentiment"]
-    Y = pd.DataFrame(target)
-    labels = le.fit_transform(Y)
-    return labels
-
-def include_word(full_phrase, keywords):
-    return 1 if full_phrase.lower() in keywords else 0
-
-def fill_columns(columns_words):
-    for key, keywords in columns_words.items():
-        df[key] = df["content"].apply(lambda full_phrase : include_word(full_phrase, keywords))
-
-
-fill_columns(columns_words)
-df.head()
+# TODO: FLask Server
