e581eaab1e0bd4a237641014bb5ea7d17c6d793d
LucaFreire <lucasrodifreire@gmail.com>
2023-11-14 11:47:47 -0300

    ToFix: include_words

--- 02 - Application/IA/IA.py
+++ 02 - Application/IA/IA.py
@@ -10,11 +10,13 @@ df = pd.read_csv("data\emotions.csv")
 cry_words = ["cry", "tears", "sob"]
 sadness_words = ["sad", "bad", "waiting"]
 happy_words = ["happy", "happiness", "sob" ]
+love_words = ["love"]
 
 columns_words = {
     "cry" : cry_words,
     "happy": happy_words,
-    "sadness": sadness_words
+    "sadness": sadness_words,
+    "love": love_words
 }
 
 def encode_sentiment():
@@ -24,12 +26,13 @@ def encode_sentiment():
     labels = le.fit_transform(Y)
     return labels
 
-def include_word(full_phrase, keywords):
-    return 1 if full_phrase.lower() in keywords else 0
+def include_word(full_phrase, vector_keywords):
+    return 1 if full_phrase.lower() in vector_keywords else 2
 
 def fill_columns(columns_words):
     for key, keywords in columns_words.items():
         df[key] = df["content"].apply(lambda full_phrase : include_word(full_phrase, keywords))
 
-
 fill_columns(columns_words)
+print(df["content"][0])
+print(df["sadness"][0])
