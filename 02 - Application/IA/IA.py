3caa88d2e6e59680b9b5894eade550dcbae6ac7b
LucaFreire <lucasrodifreire@gmail.com>
2023-11-14 11:18:01 -0300

    add: fill_columns to use the naive bayes algorithm

--- 02 - Application/IA/IA.py
+++ 02 - Application/IA/IA.py
@@ -1,45 +1,61 @@
 
-if __name__ == "IA":
-    import numpy as np
-    import pandas as pd
-    import os
-    from sklearn.tree import DecisionTreeClassifier, plot_tree
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import accuracy_score, confusion_matrix
-    from joblib import load, dump
-    from sklearn.preprocessing import LabelEncoder
-    df = pd.read_csv("IA\data\emotions.csv")
-
-def show(num):
-    df.head(num)
-
-def data_cleanup():
-    df.fillna(0)
-    le = LabelEncoder()
-    target = df["sentiment"]
-    Y = pd.DataFrame(target)
-    labels = le.fit_transform(Y)
-    return labels
-
-def training():
-    data = df[["content"]]
-
-    X = pd.DataFrame(data)
-    Y = data_cleanup()
-
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.01, random_state=800)
-    model = DecisionTreeClassifier()
-    model.fit(X_train, Y_train)
-    Y_pred = model.predict(X_test)
-    accuracy = accuracy_score(Y_test, Y_pred)
-    print(accuracy)
 
 
 
-print("Came")
-training()
 
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.01, random_state=800)
 
+# model = DecisionTreeClassifier()
+# model.fit(X_train, Y_train)
+
+# Y_pred = model.predict(X_test)
+# accuracy = accuracy_score(Y_test, Y_pred)
+# print(accuracy)
 
 # confusion_matrix(Y_test, Y_pred)
 # plot_tree(model)
+
+
+
+
+
+# print("yeah")
+# df = pd.read_csv("data\emotions.csv")
+
+# def data_cleanup(num):
+#     df.head(num)
+#     df.fillna(0)
+
+# def encode_sentiment():
+#     le = LabelEncoder()
+#     target = df["sentiment"]
+#     Y = pd.DataFrame(target)
+#     labels = le.fit_transform(Y)
+#     return labels
+
+# def training():
+#     data_cleanup()
+#     data = df[["content"]]
+#     X = pd.DataFrame(data)
+
+#     Y = encode_sentiment()
+
+#     vectorizer = TfidfVectorizer()
+#     X_train_tfidf_vectorize = vectorizer.fit_transform(X)
+
+#     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(70), random_state=1, verbose=True)
+#     clf.fit(X_train_tfidf_vectorize, Y)
+
+#     phrase_test = ["Im very sad"]
+#     X_new_tfidf_vectorize = vectorizer.transform(phrase_test)
+
+#     pred = clf.predict(X_new_tfidf_vectorize)
+
+#     for doc, category in zip(phrase_test, pred):
+#         print('%r => %s' % (doc, category))
+# print("Came")
+# training()
+
+
+
+
