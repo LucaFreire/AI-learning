bac17f0491242224aedbdff8e544c0ae2cbeb8ba
LucaFreire <lucasrodifreire@gmail.com>
2023-11-16 09:42:06 -0300

    add: decision_tree algorithm

--- 02 - Application/IA/main.py
+++ 02 - Application/IA/main.py
@@ -1,4 +1,14 @@
 import IA
+# from flask import Flask
 
+# app = Flask(__name__)
 
-# TODO: FLask Server
+# print(__name__)
+
+# @app.route('/API/<msg>', methods = ["GET"])
+# def predict_naive_bayes(msg):
+#     return IA.predict_naive_bayes(msg)
+
+# # TODO: FLask Server
+
+# app.run(host="localhost", port=3030)
